f99ca19425f6cf02eb153a2a8b2c3d4dad5d8ece
Mikhail Mosyagin <mogbymo@yandex.ru>
2018-09-16 18:38:52 +0300

    Refactor tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -10,19 +10,28 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-TEST_FUNCTION_BEGIN(TestCompareWithPrecision) 
+TEST_FUNCTION_BEGIN(TestCompareWithPrecisionEquality)
 
-    // Equality tests
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, 0.0, 0.0) == NUtil::CR_EQUAL)
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, 0.1, 0.0) != NUtil::CR_EQUAL);
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, 0.1, 0.2) == NUtil::CR_EQUAL);
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, 0.1, 0.05) != NUtil::CR_EQUAL);
 
-    // Inequality tests
+TEST_FUNCTION_END()
+
+TEST_FUNCTION_BEGIN(TestCompareWithPrecisionInequality)
+
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, 0.1, 0.05) == NUtil::CR_LESS);
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, 0.1, 0.2) == NUtil::CR_EQUAL);
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, -0.1, -0.05) == NUtil::CR_GREATER);
     TEST_ASSERT(NUtil::CompareWithPrecision<float>(0.0, -0.1, 0.2) == NUtil::CR_EQUAL);
+
+TEST_FUNCTION_END()
+
+TEST_FUNCTION_BEGIN(TestCompareWithPrecision)
+
+    TEST_ASSERT(TestCompareWithPrecisionEquality());
+    TEST_ASSERT(TestCompareWithPrecisionInequality());
 
 TEST_FUNCTION_END()
 
@@ -43,12 +52,12 @@ TEST_FUNCTION_BEGIN(TestLinearSolver)
             NQSolver::TRoots<float>{-1.0});
     TEST_ASSERT(NQSolver::SolveLinearEquation<float>(0.0, 1.0) ==
             NQSolver::TRoots<float>{});
-    TEST_ASSERT_THROW(NQSolver::SolveLinearEquation<float>(0.0, 0.0), 
+    TEST_ASSERT_THROW(NQSolver::SolveLinearEquation<float>(0.0, 0.0),
             NQSolver::TInfinitelyManyRootsException);
 
 TEST_FUNCTION_END()
 
-TEST_FUNCTION_BEGIN(TestQuadraticSolver)
+TEST_FUNCTION_BEGIN(TestQuadraticSolverDegenerateCase)
 
     TEST_ASSERT(NQSolver::SolveQuadraticEquation<float>(0.0, 1.0, 0.0) ==
             NQSolver::TRoots<float>{0.0});
@@ -57,12 +66,23 @@ TEST_FUNCTION_BEGIN(TestQuadraticSolver)
     TEST_ASSERT_THROW(NQSolver::SolveQuadraticEquation<float>(0.0, 0.0, 0.0),
             NQSolver::TInfinitelyManyRootsException);
 
+TEST_FUNCTION_END()
+
+TEST_FUNCTION_BEGIN(TestQuadraticSolverCommonCase)
+
     TEST_ASSERT(NQSolver::SolveQuadraticEquation<float>(1.0, -3.0, 2.0) ==
             NQSolver::TRoots<float>({1.0, 2.0}));
     TEST_ASSERT(NQSolver::SolveQuadraticEquation<float>(1.0, -2.0, 1.0) ==
             NQSolver::TRoots<float>{1.0});
     TEST_ASSERT(NQSolver::SolveQuadraticEquation<float>(1.0, 1.0, 1.0) ==
             NQSolver::TRoots<float>{});
+
+TEST_FUNCTION_END()
+
+TEST_FUNCTION_BEGIN(TestQuadraticSolver)
+
+    TEST_ASSERT(TestQuadraticSolverDegenerateCase());
+    TEST_ASSERT(TestQuadraticSolverCommonCase());
 
 TEST_FUNCTION_END()
 
